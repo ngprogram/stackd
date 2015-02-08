18641972b6a9fbf2b975714ec9334a6786a95a85
Caroline Wong <carowong5@gmail.com>
2015-02-07 22:00:17 -0800

    (feat) Angular structure and GET request

---
+++ server/aggregator/aggregatorController.js
@@ -0,0 +1,20 @@
+var aggregator = function() {
+var storage = [];
+
+  var add = function(val) {
+    storage.push(val);
+    
+  };
+
+  var aggregate = function() {
+
+
+  };
+
+  var clear = function() {
+    storage = [];
+  };
+};
+
+
+module.exports = aggregator;
