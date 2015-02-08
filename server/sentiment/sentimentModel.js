4d3f270026a6e2e628b88ad45404ca77ff1b88e2
Caroline Wong <carowong5@gmail.com>
2015-02-07 23:15:23 -0800

    (feat) aggregatorController functions added

--- server/sentiment/sentimentModel.js
+++ server/sentiment/sentimentModel.js
@@ -6,9 +6,37 @@ var sentimentSchema = new Schema({
   sentiment: String,
   rating: String,
   score: Number,
-  topic: String,
-  count: Number
+  topic: String
 });
 
 // compile message schema into a message model
 module.exports = mongoose.model('Sentiment', sentimentSchema);
+
+
+
+// var total = [
+//   {
+//     sentiment: 'like',
+//     rating: 'positive',
+//     score: 0.3,
+//     topic: 'react'
+//   },
+//   {
+//     sentiment: 'like',
+//     rating: 'positive',
+//     score: 0.6,
+//     topic: 'node'
+//   },
+//   {
+//     sentiment: 'awesome',
+//     rating: 'negative',
+//     score: 0.9,
+//     topic: 'angular'
+//   },
+//   {
+//     sentiment: 'huhhh',
+//     rating: 'pos',
+//     score: .3,
+//     topic: 'react'
+//   }
+// ];
