956de2d26b908b72000f245370c57766f9df64f4
Alex Zai <azai91@gmail.com>
2015-02-16 09:09:58 -0800

    (feat) integrate spellcheck and nltk controller

--- server/util/nltkController.js
+++ server/util/nltkController.js
@@ -3,84 +3,54 @@ var request = Promise.promisify(require('request'));
 var sentimentController = Promise.promisifyAll(require('../sentiment/sentimentController'));
 var config = require('config');
 var _apiKey = config.get('nltk');
+var spellCheckerController = Promise.promisifyAll(require('./spellCheckerController'));
 
 var nltkController = {};
 nltkController.getSentimentsSync = getSentimentsSync;
 
 function getSentimentsSync(comment) {
   var text = comment.text;
-  var title = comment.title;
-  var time = comment.date;
-  var commentId = comment.commentId;
-  var parameters = {text: text, language: 'eng', apikey: _apiKey};
-  var queryString = generateQuery(text);
-  console.log('text', text);
-  console.log('apikey', _apiKey);
-  var options = {
-    uri: "https://japerk-text-processing.p.mashape.com/sentiment/",
-    method: "POST",
-    headers: {
-      "X-Mashape-Key" : _apiKey,
-      "Content-Type" : "application/x-www-form-urlencoded",
-      "Accept": "application/json",
-    },
-    form: {"language": "english", "text" : text}
-  };
 
-  return request(options)
-    .spread(function (response, body) {
-      console.log('hello');
-      console.log(body);
-      return parseSentiments(JSON.parse(body), comment, title, time, commentId);
+  return spellCheckerController.correctSentence(text)
+    .then(function(correctSentence) {
+      var options = {
+        uri: "https://japerk-text-processing.p.mashape.com/sentiment/",
+        method: "POST",
+        headers: {
+          "X-Mashape-Key" : _apiKey,
+          "Content-Type" : "application/x-www-form-urlencoded",
+          "Accept": "application/json",
+        },
+        form: {"language": "english", "text" : correctSentence}
+      };
+
+      return request(options)
+        .spread(function (response, body) {
+          console.log('hello');
+          console.log(body);
+          return processSentiment(JSON.parse(body), comment);
+        })
     })
     .then(null, function(err) {
       console.log('error with nltk request', err);
     });
+
 }
 
-function parseSentiments(sentiments, comment, title, time, commentId) {
-  var sentimentsArr = [];
-  var positiveSentiments = sentiments.positive;
-  var negativeSentiments = sentiments.negative;
-
-  if (positiveSentiments && positiveSentiments.length > 0) {
-    for (var i = 0; i < positiveSentiments.length; i++) {
-      sentimentsArr.push(processSentiment(positiveSentiments[i], 'positive', comment, title, time, commentId));
-    }
-  }
-  if (negativeSentiments && negativeSentiments.length > 0) {
-    for (var i = 0; i < negativeSentiments.length; i++) {
-      sentimentsArr.push(processSentiment(negativeSentiments[i], 'negative', comment, title, time, commentId));
-    }
-  }
-
-  return sentimentsArr;
-}
-
-function processSentiment(sentiment, rating, comment, title, time, commentId) {
+function processSentiment(sentiment, comment) {
+  sentiment = sentiment.probability;
+  console.log(sentiment);
   var sentimentObj = {};
 
-  sentimentObj.sentiment = sentiment.sentiment;
-  sentimentObj.rating = rating;
-  sentimentObj.score = sentiment.score;
-  sentimentObj.title = title;
+  sentimentObj.positive = sentiment.pos;
+  sentimentObj.neutral = sentiment.neutral;
+  sentimentObj.title = comment.title;
   sentimentObj.comment = comment.text;
-  sentimentObj.commentId = commentId;
-  sentimentObj.date = time;
-  sentimentObj.author = comment.by;
+  sentimentObj.commentId = comment.commentId;
+  sentimentObj.time = comment.time;
+  sentimentObj.by = comment.by;
 
   return sentimentObj;
-}
-
-function generateQuery(text) {
-  var queryString = '?text=';
-  var textArray = text.split(' ');
-
-  for (var i = 0; i < textArray.length; i++) {
-    queryString += ('+' + textArray[i]);
-  }
-  queryString += ('&apikey=' + _apiKey);
-  return queryString;
 }
 
 module.exports = nltkController;
