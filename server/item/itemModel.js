a74c293f9efebb0669580d101482f3248253c078
Alex Zai <azai91@gmail.com>
2015-02-17 23:05:05 -0800

    (feat) update item and sentiment model

--- server/item/itemModel.js
+++ server/item/itemModel.js
@@ -3,17 +3,19 @@ var mongoose = require('mongoose'),
 
 // define message schema
 var itemSchema = new Schema({
-  id: { type: Number, unique: true, required: true },
+  id: { type: Number, required: true },
   parent: Number,
   title: String,
   text: String,
-  source: String,
+  source: { type: String, required: true },
   type: String,
   by: String,
   time: Number,
-  score: Number,
+  upvotes: Number,
   kids: [Number]
 });
+
+itemSchema.index({ "id": 1, "source": 1 }, { unique: true });
 
 // compile message schema into a message model
 module.exports = mongoose.model('Item', itemSchema);
