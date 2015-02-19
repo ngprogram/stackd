6226aedee0034db69ba230fece220fa3d093ad6f
Jonathan Ng <jon7ng@gmail.com>
2015-02-18 18:25:19 -0700

    (fix) edit server file for deployment

--- app.js
+++ app.js
@@ -32,21 +32,21 @@ app.get('/search/:term', aggregatorController.aggregate);
 
 // There are many useful environment variables available in process.env.
 // VCAP_APPLICATION contains useful information about a deployed application.
-var appInfo = JSON.parse(process.env.VCAP_APPLICATION || "{}");
+// var appInfo = JSON.parse(process.env.VCAP_APPLICATION || "{}");
 // TODO: Get application information and use it in your app.
 
 
 // VCAP_SERVICES contains all the credentials of services bound to
 // this application. For details of its content, please refer to
 // the document or sample of each service.
-var services = JSON.parse(process.env.VCAP_SERVICES || "{}");
+// var services = JSON.parse(process.env.PORT || "{}");
 // TODO: Get service credentials and communicate with bluemix services.
 
 // The IP address of the Cloud Foundry DEA (Droplet Execution Agent) that hosts this application:
-var host = (process.env.VCAP_APP_HOST || 'localhost');
+// var host = (process.env.PORT || 'localhost');
 // The port on the DEA for communication with the application:
-var port = (process.env.VCAP_APP_PORT || 3000);
+var port = (process.env.PORT || 3000);
 // Start server
-app.listen(port, host);
+app.listen(port);
 console.log('App started on port ' + port);
 
