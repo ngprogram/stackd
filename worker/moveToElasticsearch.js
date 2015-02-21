84781364b682e437d8976f198cc23dacba5a9bda
Alex Zai <azai91@gmail.com>
2015-02-21 11:10:41 -0800

    (feat) add bulk function to escontroller

--- worker/moveToElasticsearch.js
+++ worker/moveToElasticsearch.js
@@ -1,12 +1,13 @@
 var Promise = require('bluebird');
 var sentimentController = require('../server/sentiment/sentimentController');
+var itemController = require('../server/item/itemController');
 var elasticsearchController = require('../server/elasticsearch/elasticsearchController');
 var mongoose = require('mongoose');
 var config = require('config');
 
 mongoose.connect(config.get('mongo'));
 
-function migrate() {
+function migrateSentiments() {
   sentimentController.getAllSentiments()
     .then(function(response) {
       console.log('success', response);
@@ -16,5 +17,15 @@ function migrate() {
     });
 }
 
-migrate();
+function migrateItems() {
+  itemController.getAllStories()
+    .then(function(stories) {
+      return elasticsearchController.migrate(stories, 'stories')
+    })
+    .then(null, function(err) {
+      console.log('error migrating', err);
+    });
+}
+
+migrateItems();
 
